a03c14b273d817632b9d8524f01c88ad54d23cd0
Alexey <bogwowwww@mail.ru>
2024-12-22 00:30:54 +0300

    hw_21: для функций main, catalog_categories category_detail добавили реверсы и добавили ссылки

--- python_blog/urls.py
+++ python_blog/urls.py
@@ -1,17 +1,19 @@
 # APP URLS
+app_name = 'blog'
 from django.contrib import admin
 from django.urls import path
 from python_blog.views import catalog_categories, catalog_tags, catalog_posts, category_detail, tag_detail, post_detail
 
 urlpatterns = [
+    # Каталог постов
+    path('', catalog_posts, name='posts'),
     # Категории
-    path('categories/', catalog_categories),
-    path('categories/<slug:category_slug>/', category_detail),
+    path('categories/', catalog_categories, name='categories'),
+    path('categories/<slug:category_slug>/', category_detail, name='category_detail'),
     # Теги
-    path('tags/', catalog_tags),
-    path('tags/<slug:tag_slug>/', tag_detail),
+    path('tags/', catalog_tags, name='tags'),
+    path('tags/<slug:tag_slug>/', tag_detail, name='tag_detail'),
     # Посты
-    path('', catalog_posts),
-    path('<slug:post_slug>/', post_detail),
+    path('<slug:post_slug>/', post_detail, name='post_detail'),
     
 ]
