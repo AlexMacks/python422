69da84f5398254feecf8001d0355fbf8aafa0507
Alexey <bogwowwww@mail.ru>
2025-01-07 15:36:27 +0300

    hw22: добавили подсветку активной кнопки главной страницы

--- python_blog/context_processors.py
+++ python_blog/context_processors.py
@@ -1,15 +1,16 @@
-"""
-Контекстный процессор передающий меню в каждый из шаблонов Джанго!
-Не забудьте подключить это в в settings.py -> TEMPLATES -> context_processors
-'python_blog.context_processors.menu_items', 
-"""
-MENU_ITEMS = [
+
+def menu_items(request):
+    menu = [
     {"title": "Главная", "url_name": "main"},
     {"title": "Все посты", "url_name": "blog:posts"},
     {"title": "Категории", "url_name": "blog:categories"},
     {"title": "Теги", "url_name": "blog:tags"},
 ]
-def menu_items(request):
-    return {
-        'menu_items': MENU_ITEMS
-    }
+    
+    # Определяем активный пункт меню
+    current_url_name = request.resolver_match.view_name
+    
+    for item in menu:
+        item['is_active'] = current_url_name == item['url_name']
+    
+    return {'menu_items': menu}
