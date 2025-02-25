b8d7d3f0266edf76b5397fd5ed8d15319cbbb15f
Alexey <bogwowwww@mail.ru>
2025-02-25 11:40:37 +0300

    hw 26: добавили в админку возможность смотреть статус, а также добавили кнопку создать пост

--- python_blog/context_processors.py
+++ python_blog/context_processors.py
@@ -1,14 +1,20 @@
+"""
+Контекстный процессор передающий меню в каждый из шаблонов Джанго!
+Не забудьте подключить это в в settings.py -> TEMPLATES -> context_processors
+'python_blog.context_processors.menu_items', 
+"""
+
 
 def menu_items(request):
     menu = [
-    {"title": "Главная", "url_name": "main"},
-    {"title": "О нас", "url_name": "about"},
-    {"title": "Все посты", "url_name": "blog:posts"},
-    {"title": "Категории", "url_name": "blog:categories"},
-    {"title": "Теги", "url_name": "blog:tags"},
-]
+        {"title": "Главная", "url_name": "main"},
+        {"title": "О нас", "url_name": "about"},
+        {"title": "Все посты", "url_name": "blog:posts"},
+        {"title": "Создать пост", "url_name": "blog:post_create"},
+        {"title": "Категории", "url_name": "blog:categories"},
+        {"title": "Теги", "url_name": "blog:tags"},
+    ]
     
-    # Определяем активный пункт меню
     current_url_name = request.resolver_match.view_name
     
     for item in menu:
